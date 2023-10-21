8c4cb1db65701f0d354958f757a3d10d8c22e95f
jairoaussie <jairo.bilbao@coderacademy.edu.au>
2023-10-21 10:59:50 +1000

    callback functions

--- functions.js
+++ functions.js
@@ -62,8 +62,60 @@ const greeterWithDefault = (customMessage = "to the lesson") => {
 // functions not executed, just copied around.
 //let storedCustomMessage = greeterWithDefault;
 
-let storedCustomMessage = greeterWithDefault("to the fat arrow world");
+// let storedCustomMessage = greeterWithDefault("to the fat arrow world");
 
-console.log(`The returned value from the ES6 function is ${storedCustomMessage}`);
+// console.log(`The returned value from the ES6 function is ${storedCustomMessage}`);
 // greeterWithDefault();
 // greeterWithDefault("to the fat arrow functionality")
+
+//IIFE
+/**
+ * 
+ */
+// ((num1 = 5, num2 = 12) => {
+//     console.log(`${num1 * num2}`);
+// })(3,5);
+
+
+//Callback
+// function addition (num1, num2) {
+//     console.log(`the result is ${num1 + num2}`)
+//     //return num1 + num2;
+// }
+
+// function printStuff(value1, value2, callback){
+//     console.log(typeof(callback));
+//     callback(value1, value2);
+// }
+
+// function multiplcation (num1, num2) {
+//     console.log(`the result is ${num1 * num2}`)
+// }
+
+// function greeter (word1, word2) {
+//     console.log(`${word1} ${word2}`)
+// }
+    
+
+// printStuff(4, 3, addition);
+// printStuff(4, 3, multiplcation);
+// printStuff("Hello", "World", greeter);
+
+// let array = [1, 4, 7, 9, 10, 14, 16];
+
+// function printArray(item, i, a){
+//     console.log(item, i, a[i])
+// }
+// // // create the anonymous function within the forEach
+// // array.forEach((item, i, a) => console.log(item, i, a[i]));
+
+// // // Use printArray function as a callback, 
+// // // both of them will executed in each iteration of the array loop
+// // array.forEach(printArray);
+
+// const button = document.getElementById("button")
+
+// button.addEventListener("click", () => {
+//     console.log("Hey, you actually clicked the button!")
+// });
+
